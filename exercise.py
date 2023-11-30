7dd181cc1578e5a680a333ce20fca0222079e10d
Laleti Cynthia <+>
2023-11-30 16:20:00 +0300

    codility exercise final

--- exercise.py
+++ exercise.py
@@ -2,12 +2,13 @@ def longest_switching_slice(A):
     max_length = 1
     current_length = 1
     for i in range(1, len(A)):
-        if A[i] != A[i-1]
-        current_length +=1
-    else:
-        current_length=1
-    max_length = max(max_length, current_length)
+        if A[i] != A[i - 1]:
+            current_length += 1
+        else:
+            current_length = 1
+        max_length = max(max_length, current_length)
     return max_length
-A=[7,8,7,8]
-longest_slice = (longest_switching_slice)
+
+A = [7, 8, 7, 8]
+longest_slice = longest_switching_slice(A)
 print(longest_slice)
